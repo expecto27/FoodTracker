cb0ba94aef94675374a1a4b161806e4cda41a24e
expecto27 <expeo@vk.com>
2024-03-29 22:56:21 +0800

    bottom navigation part

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,11 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
 
+    //nav
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("com.google.android.material:material:1.11.0")
+
     //di
     implementation("com.google.dagger:hilt-android:2.51")
     kapt("com.google.dagger:hilt-compiler:2.51")
